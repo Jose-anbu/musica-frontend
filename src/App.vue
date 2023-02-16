040ea86765f8b05b198784b50c3449eeaf9aebd0
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-02-16 00:51:26 -0300

    Se agregan links de fuentes y bootstrap. Se crean estilos en App y Layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    
+  <div class="app">
+    <Layout/>
+
   </div>
 </template>
 
 <script>
+  import Layout from './components/Layout.vue'
+  export default{
+    components:{
+      Layout
+    }
+  }
+  
 
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+}
 
+body{
+  font-family: 'Gloria Hallelujah', cursive;
+}
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <nav class="navbar bg-dark">
+            <div class="px-5 d-flex">
+                <img src="../assets/logo.png" alt="Logo" class="imagenLogo__nav">
+                <div class="texto__nav navbar-brand">
+                    <a>Lo Vueno &#128526;</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+nav{
+    box-shadow: 5px 10px 20px grey;
+    position: sticky;
+}
+
+.imagenLogo__nav{
+    width: 8%;
+}
+
+.texto__nav{
+    display: flex;
+    color: #00bf63;
+    text-shadow: 0px 0px 10px white;
+    align-items: center;
+}
+</style>
