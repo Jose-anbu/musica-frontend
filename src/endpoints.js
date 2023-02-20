65626ef609b926fda5da623a36d428668bf5e3c4
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-02-20 03:53:29 -0300

    En endpoints.js se agregan metodos getArtista y getCancionesArtista. En Artista.vue se agrega codigo para el componente, en template, script y estilos.

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,11 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
-export default{
-    async getListaArtistas(offset){
-        return await axios.get('http://localhost:5000/artistas')
+export default {
+  async getListaArtistas(offset) {
+    return await axios.get("http://localhost:5000/artistas");
+  },
+  async getArtista(id) {
+    let artista = await axios.get(`http://localhost:5000/artistas/${id}`);
+    return artista.data;
+  },
+  async getCancionesArtista(canciones_) {
+    let canciones = [];
+    let cancion = {};
+    for (let i = 0; i < canciones_.length; i++) {
+      cancion = await axios.get(`http://localhost:5000/canciones/${canciones_[i]}`);
+      canciones.push(cancion.data);
     }
-}
-
-// axios.get('http://localhost:5000/artistas/').then((lista)=>{
-    //   this.artistas=lista.data
-    // }),
+    return canciones;
+  },
+};
--- src/pages/Artista.vue
+++ src/pages/Artista.vue
@@ -1,13 +1,82 @@
 <template>
-    <div>
-        <h1>Página de Artista</h1>
+  <div>
+    <h1 class="pb-4">{{ artista.nombre }}</h1>
+    <div class="d-flex">
+      <div class="contenedorImagen me-4">
+        <img :src='imagenArtista' class="img-thumbnail" :alt='nombreArtista'>
+      </div>
+      <div class="seccionDetalles">
+        <ul>
+          <li>Género: {{ artista.genero }}</li>
+          <li>País: {{ artista.pais }}</li>
+          <li>Biografía: {{ artista.biografia }}</li>
+        </ul>
+      </div>
     </div>
+    <div class="seccionCanciones">
+      <h3 class="py-4">Canciones</h3>
+      <ul>
+        <li v-for="cancion in canciones">{{ cancion.nombre }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
+
 <script>
+import endpoints from '../endpoints'
+
 export default {
-    
+  name: 'Artista',
+  data() {
+    return {
+      id: '',
+      artista: {},
+      canciones: []
+    }
+  },
+  created() {
+    this.id = this.$route.params.id.toString();
+    endpoints.getArtista(this.id).then((artista) => {
+      this.artista = artista
+      endpoints.getCancionesArtista(this.artista.canciones)?.then((canciones_) => {
+        console.log(canciones_)
+        this.canciones = canciones_
+      })
+      return {
+        ...this.artista,
+        ...this.canciones
+      }
+    })
+  },
+  computed: {
+    nombreArtista() {
+      return this.artista.nombre
+    },
+    imagenArtista() {
+      return this.artista.imagen
+    }
+  },
 }
 </script>
+
 <style>
-    
+.contenedorImagen {
+  width: 40%;
+  overflow: hidden;
+}
+
+.seccionDetalles {
+  width: 50%;
+}
+
+.seccionDetalles li {
+  padding-bottom: 10px;
+  list-style: circle;
+}
+
+.seccionCanciones li {
+  margin-bottom: 10px;
+  list-style: inside;
+  list-style-type: none;
+}
 </style>
