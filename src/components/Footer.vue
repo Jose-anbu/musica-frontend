d07ba4ae4ba9a1b70a9a6c571449e0de8f3cfda0
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-02-21 03:08:17 -0300

    Se agregan enlaces a LinkedIn y GitHub en Footer y se agregan estilos a dicho componente

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="bg-dark text-light prueba p-3 fixed-bottom">
+    <div class="bg-dark text-light contenedorFooter pt-3 fixed-bottom">
         <div>
-            <!-- <router-link ></router-link>
-            <router-link ></router-link>
-            <router-link ></router-link> -->
-            
+            <a href="https://www.linkedin.com/in/joseantonioburgos/" target="_blank" title="LinkedIn"><img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt="LinkedIn" class="icono"></a>
+            <a href="https://github.com/Jose-anbu" class="mx-3" target="_blank" title="GitHub"><img src="https://cdn-icons-png.flaticon.com/512/3291/3291695.png" alt="GitHub" class="icono"></a>
         </div>
-        <p class="text-center">&copy; Todos los derechos reservados - José Antonio Burgos - 2023</p>
+        <p class="pt-2">&copy; Todos los derechos reservados - José Antonio Burgos - 2023</p>
     </div>
 </template>
 
 <style>
-.prueba{
+.contenedorFooter{
     box-shadow: 5px -10px 20px grey;
-    
+    font-size: 0.9rem;
+    text-align: center;
+}
+.contenedorFooter img{
+    height: 20px;
+}
+.icono{
+    background-color: white;
+    border-radius: 5px;
+    transition: 400ms;
+}
+.icono:hover{
+    box-shadow: 0px 0px 5px grey;
+    transform: scale(1.2);
 }
 </style>
